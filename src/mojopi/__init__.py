5965b447cd3c8cf9ab4cc49051d34bcfdd4d93a7
Drunkwcodes <drunkwcodes@gmail.com>
2023-08-04 00:20:30 +0800

    bump version

--- src/mojopi/__init__.py
+++ src/mojopi/__init__.py
@@ -1,4 +1,4 @@
 from .models import add_project, add_ring, add_user, is_valid_email, is_valid_username
 from .utils import InvalidInputError, hash_password, is_email, verify_password
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
